88ed7f16061b70efec19ea8e04b30f3155425efe
Yoeri Joëll Moens <yoerimoens@gmail.com>
2024-10-08 20:27:07 +0200

    Added the factors function

--- the-haskell-road-to-logic-maths-and-programming/prime.hs
+++ the-haskell-road-to-logic-maths-and-programming/prime.hs
@@ -1,9 +1,18 @@
 module GS where
 
+divides :: Integral a => a -> a -> Bool
 divides d n = rem n d == 0
 
+ld :: Integral t => t -> t
 ld n = ldf 2 n
 
+ldf :: Integral t => t -> t -> t
 ldf k n | divides k n = k
         | k^2 > n = n
         | otherwise = ldf (k+1) n
+
+factors :: Integer -> [Integer]
+factors n | n < 0 = error "Cannot be less than zero"
+          | n == 1 = []
+          | otherwise = p : factors (n `div` p)
+          where p = ld n
